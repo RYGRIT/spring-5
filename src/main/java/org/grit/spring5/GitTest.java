f80f1d981bb50799ba856ceb56af4dd6a861b0af
Hubo <468805354@qq.com>
2021-09-03 16:02:57 +0800

    second commit

---
+++ src/main/java/org/grit/spring5/GitTest.java
@@ -0,0 +1,13 @@
+package org.grit.spring5;
+
+/**
+ * @author Grit
+ * @date 2021-09-03
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Git Test2");
+        System.out.println("Git Test2");
+        System.out.println("Git Test2");
+    }
+}
